0973737b44ab5b8a9e7d3419f966f44017c03e35
Tyler Crawford <tjc4jc@virginia.edu>
2016-04-21 12:01:14 -0400

    Cleaned up code and put database access in config.php. Just require that in the top of new files

--- index.php
+++ index.php
@@ -1,35 +1,23 @@
-<html>
-<head>
-    <script src="http://code.jquery.com/jquery-latest.js"></script>
-</head>
-
 <?php
-$con = new mysqli('stardock.cs.virginia.edu', 'cs4750sam4ku', 'scott', 'cs4750sam4ku');
-if (mysqli_connect_errno())
-{
-	echo "Failed to connect to MYSQL: " . mysqli_connect_error();
-}
-echo "hello world";
-$sql = "SELECT * FROM Person";
-// // $sql = "SELECT Password FROM Admins";
-// $result = $con->query($sql);
-// echo $result;
-
-
-$result = mysqli_query($con, $sql);
-
-while($row = mysqli_fetch_array($result)) {
-    echo $row['p_name'];
-    echo " " . $row['birthyear'];
-    echo "<br>";
-}
-echo "bye";
-
-// // check to see if the password matches the password stored in the database
-// // Both passwords are hashed
-// foreach ($result as $user):
-// 	$pass = $user["Password"];
-// 	echo $pass;
-// mysqli_close($con);
+    require_once("config.php");
 ?>
+<html>
+    <head>
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+
+    <body>
+    <?php
+        $sql = "SELECT * FROM Person";
+
+        $result = mysqli_query($con, $sql);
+
+        while($row = mysqli_fetch_array($result)) {
+            echo $row['p_name'];
+            echo " " . $row['birthyear'];
+            echo "<br>";
+        }
+    ?>
+    </body>
 </html>
--- kingdoms.php
+++ kingdoms.php
@@ -1,33 +1,27 @@
-<html>
-
 <?php
-$con = new mysqli('stardock.cs.virginia.edu', 'cs4750sam4ku', 'scott', 'cs4750sam4ku');
-if (mysqli_connect_errno())
-{
-	echo "Failed to connect to MYSQL: " . mysqli_connect_error();
-}
-echo "hello world <br>";
-$sql = "SELECT * FROM Kingdom";
-// // $sql = "SELECT Password FROM Admins";
-// $result = $con->query($sql);
-// echo $result;
-
-
-$result = mysqli_query($con, $sql);
-
-while($row = mysqli_fetch_array($result)) {
-    echo "<a href='view_kingdom.php?kingdom=" , $row['k_name'] , "'>View</a>";
-    echo $row['k_name'];
-    echo " " . $row['leige_lord'];
-    echo "<br>";
-}
-echo "bye";
-
-// // check to see if the password matches the password stored in the database
-// // Both passwords are hashed
-// foreach ($result as $user):
-// 	$pass = $user["Password"];
-// 	echo $pass;
-// mysqli_close($con);
+    require_once("config.php");
 ?>
+
+<html>
+    <head>
+    </head>
+
+    <body>
+        <?php
+            $sql = "SELECT * FROM Kingdom";
+            // // $sql = "SELECT Password FROM Admins";
+            // $result = $con->query($sql);
+            // echo $result;
+
+
+            $result = mysqli_query($con, $sql);
+
+            while($row = mysqli_fetch_array($result)) {
+                echo "<a href='view_kingdom.php?kingdom=" , $row['k_name'] , "'>View</a>";
+                echo $row['k_name'];
+                echo " " . $row['leige_lord'];
+                echo "<br>";
+            }
+        ?>
+    </body>
 </html>
