64fb76929afba35dc8bf1cecf30bd98cb3488b08
kww9dt <kww9dt@virginia.edu>
2016-04-21 18:33:50 -0400

    AJAX on Search

--- autocomplete.php
+++ autocomplete.php
@@ -1,17 +1,15 @@
 <?php
-
 require_once("config.php");
-$db_handle = new DBController();
-if(!empty($_POST["keyword"])) {   
-$sql = "SELECT * FROM Person WHERE p_name like '".$_POST["keyword"]. "%'";
-$results = $db_handle->runQuery($sql);
-if(!empty($results)) {
+if(!empty($_POST["keyword"])) {
+$query ="SELECT * FROM Person WHERE p_name like '" . $_POST["keyword"] . "%' ORDER BY p_name";
+$result = mysqli_query($con, $query);
+if(!empty($result)) {
 ?>
 <ul id="name-list">
 <?php
 foreach($result as $name) {
 ?>
-<li onClick="selectName('<?php echo $name["name"]; ?>');"><?php echo $name["name"]; ?></li>
+<li onClick="selectPerson('<?php echo $name["p_name"]; ?>');"><?php echo $name["p_name"]; ?></li>
 <?php } ?>
 </ul>
 <?php } } ?>
