ee3b55c9422755103a96c7fda8b832ea3eb9be8a
kww9dt <kww9dt@virginia.edu>
2016-04-21 20:09:26 -0400

    Stuck

--- search.php
+++ search.php
@@ -1,46 +1,30 @@
 <?php
     require_once("config.php");
 ?>
-<h3>FORM</h3>
-<form name = "searchform" action = "search.php" method = "POST">
-        <select name="drop">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-          <option value="mercedes">Mercedes</option>
-          <option value="audi">Audi</option>
-        </select>
-        Text: <br/><input type = "text" name = "text" maxlength = "50" required><br /> <br/>
-    <input type = "submit" class="btn btn-default" name = "submit" value = "Submit">
-</form>
 
-<html>
-    <head>
-        <script src="http://code.jquery.com/jquery-latest.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    </head>
+<?php 
 
-    <body>
-        <?php
-            $sql = "SELECT * FROM Person";
 
-            $result = mysqli_query($con, $sql);
-        ?>
-        <h3>SEARCH FORM</h3>
-        <form name = "searchform" action = "search.php" method = "POST">
-            <!-- Username: <br/><input type = "text" name = "username" maxlength = "50" required><br />  -->
-            Text: <br/><input type = "text" name = "text" maxlength = "50" required><br /> <br/>
-            <input type = "submit" class="btn btn-default" name = "submit" value = "Submit">
-        </form>
-        <?php
-            while($row = mysqli_fetch_array($result)) {
-                echo $row['p_name'];
-                echo " " . $row['birthyear'];
-                echo "<br>";
-            }
-        ?>
-    </body>
-</html>
+    foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
 
+
+?>
+
+<?php
+$query ="SELECT * FROM '" . $_POST["question"] . "'";
+echo $query;
+$result = mysqli_query($con, $query);
+?>
+            
 <?php
     mysqli_close($con);
 ?>
